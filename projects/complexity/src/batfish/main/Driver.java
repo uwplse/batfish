569d822f1d1849adf50e1d2f77eda936a8444391
wenfei-wu <wenfeiwu@cs.wisc.edu>
2014-12-03 14:14:44 -0600

    input args

--- projects/complexity/src/batfish/main/Driver.java
+++ projects/complexity/src/batfish/main/Driver.java
@@ -6,6 +6,11 @@ import java.util.Map;
 public class Driver {
 
 	public static void main(String []args) throws Exception{
+		if(args.length!=1){
+			System.out.println("Usage: complexity <path to configs>");
+			System.exit(1);
+		}
+		
 		Batfish b=new Batfish();
 		String path;
 		path = args[0];
